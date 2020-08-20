efa0e6cd900a5531e8c634ec3da27c2432fb411e
Will Taylor <wat23@bath.ac.uk>
2020-08-20 17:48:16 +0100

    init

---
+++ scripts/index.js
@@ -0,0 +1,38 @@
+function verifySectionId(id) {
+  return id.startsWith('#') ? id.slice(1) : id;
+}
+
+function isElementInViewport(el) {
+  // from - https://stackoverflow.com/questions/123999/how-can-i-tell-if-a-dom-element-is-visible-in-the-current-viewport/7557433#7557433
+  // Special bonus for those using jQuery
+  if (typeof jQuery === 'function' && el instanceof jQuery) {
+    el = el[0];
+  }
+
+  var rect = el.getBoundingClientRect();
+
+  return (
+    rect.top >= 0 &&
+    rect.left >= 0 &&
+    rect.bottom <=
+      (window.innerHeight ||
+        document.documentElement.clientHeight) /* or $(window).height() */ &&
+    rect.right <=
+      (window.innerWidth ||
+        document.documentElement.clientWidth) /* or $(window).width() */
+  );
+}
+
+function highlightSection(sectionId = '#') {
+  const id = verifySectionId(sectionId);
+  const section = document.getElementById(id);
+
+  if (isElementInViewport(section)) {
+    section.classList.add('click-highlight');
+    setTimeout(function () {
+      section.classList.remove('click-highlight');
+    }, 2500);
+  }
+}
+
+var scroll = new SmoothScroll('a[href*="#"]');
---
+++ tailwind.config.js
@@ -0,0 +1,69 @@
+module.exports = {
+  purge: [],
+  theme: {
+    backgroundColor: (theme) => ({
+      ...theme('colors'),
+      'primary-blue': '#5D8CD3',
+      'primary-blue-translucent': 'rgba(93, 140, 211, 0.98)',
+      // Job indicator
+      'job-green': '#6FCF97',
+      'job-yellow': '#FFE600',
+      'job-red': '#FF7878',
+      // Projects
+      'travelhack-blue': '#55AFE6',
+      'wesbos-yellow': '#FFC600',
+      'solar-system-blue': '#213769',
+    }),
+    textColor: (theme) => ({
+      ...theme('colors'),
+      black: '#1D1D1F',
+      white: '#FFFFFF',
+      link: '#1565C0',
+    }),
+    fontFamily: {
+      sans: [
+        '"Josefin Sans"',
+        'Roboto',
+        'Oxygen',
+        'Ubuntu',
+        'Cantarell',
+        '"Open Sans"',
+        '"Helvetica Neue"',
+        'sans-serif',
+      ],
+      cursive: ['"Gloria Hallelujah"', 'cursive'],
+    },
+    extend: {
+      flex: {
+        '2': '2 2 0%',
+      },
+      maxHeight: {
+        '32': '32px',
+        '64': '64px',
+        '128': '128px',
+      },
+      maxWidth: {
+        '40': '40%',
+        '110': '110%',
+        '50': '50%',
+        '2/3': '66.66%',
+        '80': '80%',
+        '680': '680px',
+      },
+      minHeight: {
+        '64': '64px',
+        '128': '128px',
+      },
+      minWidth: {
+        '650': '650px',
+        '110': '110%',
+        '700': '700px',
+        '1024': '1024px',
+      },
+    },
+  },
+  variants: {
+    display: ['responsive', 'hover', 'focus'],
+  },
+  plugins: [],
+};
