614d92c278b117bd1d456c2d6ad2202085c9e685
Will Taylor <will.taylorxv@gmail.com>
2023-11-11 11:47:45 +0000

    A new start

---
+++ tailwind.config.js
@@ -0,0 +1,41 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        "zoe-cream": "rgb(251, 248, 243)",
+        "zoe-text": "rgb(45, 50, 70)",
+      },
+    },
+    fontFamily: {
+      sans: [
+        "Open sans",
+        "ui-sans-serif",
+        "system-ui",
+        "-apple-system",
+        "BlinkMacSystemFont",
+        "Segoe UI",
+        "Roboto",
+        "Helvetica Neue",
+        "Arial",
+        "Noto Sans",
+        "sans-serif",
+        "Apple Color Emoji",
+        "Segoe UI Emoji",
+        "Segoe UI Symbol",
+        "Noto Color Emoji",
+      ],
+      serif: [
+        "Lora",
+        "ui-serif",
+        "Georgia",
+        "Cambria",
+        "Times New Roman",
+        "Times",
+        "serif",
+      ],
+    },
+  },
+  plugins: [],
+};
